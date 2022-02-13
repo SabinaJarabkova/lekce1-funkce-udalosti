8c5b7000974cc6de6f407fd783b29173c3f3a931
Sabina Jarabkova <jarabkova.sabina@gmail.com>
2022-02-13 10:49:11 +0100

    domaci ukol

--- main.js
+++ main.js
@@ -8,14 +8,34 @@ document.querySelector("#vysledek").innerHTML = secti(4, 5);
  * @param {int} a
  * @param {int} b
  */
+
+let a = 5;
+let b = 2;
+
 function secti(a, b) {
   let c = a + b;
   return c;
 }
 
+
+
+function priKliknuti(){
+  let ctverecek;
+  ctverecek = document.querySelector('.ctverecek');
+  ctverecek.style.backgroundColor = 'green';
+}
+
+function celkem(selector) {
+  document.querySelector(selector).innerHTML = secti();
+}
+
 /**
  * Upozorní uživatele při spuštění.
  */
-function upozorni() {
+ function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log('Gratulace, právě jsi spustila tuto funkci!');
+  let ctverecek = document.querySelector('.ctverecek');
+  ctverecek.style.width = '350px';
+  ctverecek.innerHTML = '<strong> Gratulace</strong>, právě jsi spustila tuto funkci!';
 }
